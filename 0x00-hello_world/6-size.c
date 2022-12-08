f0ba3762af1508879081fe6ccc5740ef56cb30e2
Henok Aklilu <116839985+henok-aklilu@users.noreply.github.com>
2022-12-08 16:40:00 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
- * Return: Always 0 (Success)
+ *main -Entry point 
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -11,6 +11,5 @@ printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long  int));
 printf("Size of a float: %zu byte(s)\n", sizeof(float));
-   return (0);	
-}
-	
+return (0);	
+}	
