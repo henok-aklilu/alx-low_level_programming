0c66ffc38d93c20d630ebab7b17a350177bbde07
Henok Aklilu <116839985+henok-aklilu@users.noreply.github.com>
2022-12-22 08:04:12 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
-
-#endif
+#ifndef MAIN_H 
+#define MAIN_H 
+  
+#include <stdio.h> 
+  
+ void positive_or_negative(int i); 
+ int largest_number(int, int, int); 
+ void print_remaining_days(int, int, int); 
+ int convert_day(int, int); 
+  
+ #endif /* MAIN_H */
