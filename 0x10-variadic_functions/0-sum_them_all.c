5b87701a8b641da5d183d72ceb00821b957d70be
henok-aklilu <henokaklilu065@gmail.com>
2023-01-18 17:13:57 -0800

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,8 +1,9 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
-*sum_them_all - sums all the passed integer arguments
-*@n: number of arguments that is passed
+*sum_them_all - sums all its parameters
+*@n: first parameter
 *
 *Return: 0 if n is null
 * or sum of parameters in other case
@@ -10,21 +11,21 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
-	count i = 0;
-	va_list theList;
 
-	if (n == 0)
-	{
-		return (0);
-	}
+va_list li;
+int sum = 0;
+unsigned int i;
 
-	va_start(theList, n);
-	for (; i < n; i++)
-	{
-		sum += va_arg(theList, int);
-	}
-	va_end(theList);
-	return (sum);
+va_start(li, n);
+
+if (n != 0)
+{
+for (i = 0; i < n; i++)
+sum += va_arg(li, int);
+}
+
+va_end(li);
+return (sum);
+
 }
 
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,19 +4,11 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-typedef struct printer{
-char *symbol;
-void (*print)(va_list arg);
-} printer_t;
-
+typedef unsigned int count;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
 void print_all(const char * const format, ...);
 
 #endif
