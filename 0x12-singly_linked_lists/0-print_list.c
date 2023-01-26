41fb5deb8090ae5f33065674948fbb459fcb5dbd
henok-aklilu <henokaklilu065@gmail.com>
2023-01-26 12:26:23 -0800

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head noe of linked list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			i++;
+			continue;
+		}
+
+		printf("[%u] %s\n", h->len, h->str);
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
+
