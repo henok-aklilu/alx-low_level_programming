6a972b1c63af308e946bce85bc43383c3bf93cb9
henok-aklilu <henokaklilu065@gmail.com>
2023-02-06 16:54:48 -0800

    update_main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <fcnt1.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
